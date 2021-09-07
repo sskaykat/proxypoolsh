b617a32a9dcdc1bae1157b4c052c34d6213e9b60
shoujiyanxishe <82391323+shoujiyanxishe@users.noreply.github.com>
2021-09-07 14:22:48 +0800

    Update install.sh

--- termux/install.sh
+++ termux/install.sh
@@ -1,1 +1,11 @@
-termux-setup-storage && pkg up -y && pkg i golang tar vim -y && wget https://github.com/Sansui233/proxypool/archive/refs/tags/v0.7.3.tar.gz && tar -zxvf v0.7.3.tar.gz && mv proxypool-0.7.3 proxypool && rm v0.7.3.tar.gz ~/proxypool/config/config.yaml ~/proxypool/config/source.yaml && wget -P ~/proxypool/config https://raw.githubusercontent.com/shoujiyanxishe/proxypoolsh/main/termux/config.yaml && wget -P ~/proxypool/config https://raw.githubusercontent.com/shoujiyanxishe/proxypoolsh/main/termux/source.yaml && wget -N "https://raw.githubusercontent.com/shoujiyanxishe/proxypoolsh/main/termux/proxypool.sh" && chmod +x proxypool.sh && rm install.sh
+pkg i golang -y
+go get -u -v github.com/Sansui233/proxypool
+proxypool=$(ls ~/go/pkg/mod/github.com/'!sansui233')
+chmod -R 777 ~/go/pkg/mod/github.com/'!sansui233'
+cp -r ~/go/pkg/mod/github.com/'!sansui233'/$proxypool ~/proxypool
+rm -rf ~/go/pkg/mod/github.com/'!sansui233' ~/proxypool/config/config.yaml ~/proxypool/config/source.yaml
+wget -P ~/proxypool/config https://raw.githubusercontent.com/shoujiyanxishe/proxypoolsh/main/termux/config.yaml
+wget -P ~/proxypool/config https://raw.githubusercontent.com/shoujiyanxishe/proxypoolsh/main/termux/source.yaml
+wget -N "https://raw.githubusercontent.com/shoujiyanxishe/proxypoolsh/main/termux/proxypool.sh"
+chmod +x proxypool.sh
+rm install.sh
