0d6694c42674429ce5f80d80a7bb561cf356664d
shoujiyanxishe <82391323+shoujiyanxishe@users.noreply.github.com>
2021-12-17 22:17:41 +0800

    Update install.sh

--- termux/install.sh
+++ termux/install.sh
@@ -1,3 +1,6 @@
+sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/termux-packages-24 stable main@' $PREFIX/etc/apt/sources.list
+sed -i 's@^\(deb.*games stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/game-packages-24 games stable@' $PREFIX/etc/apt/sources.list.d/game.list
+sed -i 's@^\(deb.*science stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/science-packages-24 science stable@' $PREFIX/etc/apt/sources.list.d/science.list
 pkg up -y
 pkg i golang -y
 go get -u -v github.com/Sansui233/proxypool
@@ -5,9 +8,9 @@ proxypool=$(ls ~/go/pkg/mod/github.com/'!sansui233')
 chmod -R 777 ~/go/pkg/mod/github.com/'!sansui233'
 cp -r ~/go/pkg/mod/github.com/'!sansui233'/$proxypool ~/proxypool
 rm -rf ~/go/pkg/mod/github.com/'!sansui233' ~/proxypool/config/config.yaml ~/proxypool/config/source.yaml
-wget -P ~/proxypool/config https://raw.githubusercontent.com/shoujiyanxishe/proxypoolsh/main/termux/config.yaml
-wget -P ~/proxypool/config https://raw.githubusercontent.com/shoujiyanxishe/proxypoolsh/main/termux/source.yaml
-wget -N "https://raw.githubusercontent.com/shoujiyanxishe/proxypoolsh/main/termux/proxypool.sh"
+config=$(curl -Ls https://raw.githubusercontent.com/shoujiyanxishe/proxypoolsh/main/termux/config.yaml) && echo "$config" > ~/proxypool/config/config.yaml
+source=$(curl -Ls https://raw.githubusercontent.com/shoujiyanxishe/proxypoolsh/main/termux/source.yaml) && echo "$source" > ~/proxypool/config/source.yaml
+echo "cd ~/proxypool && go run main.go -c ./config/config.yaml" > proxypool.sh
 chmod +x proxypool.sh
 rm install.sh
 clear
