655f56dd5b69b4b8939cae1a5ee8afd2d34aaed7
shoujiyanxishe <82391323+shoujiyanxishe@users.noreply.github.com>
2021-12-17 22:48:52 +0800

    Update install.sh

--- termux/install.sh
+++ termux/install.sh
@@ -2,14 +2,15 @@ sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/t
 sed -i 's@^\(deb.*games stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/game-packages-24 games stable@' $PREFIX/etc/apt/sources.list.d/game.list
 sed -i 's@^\(deb.*science stable\)$@#\1\ndeb https://mirrors.tuna.tsinghua.edu.cn/termux/science-packages-24 science stable@' $PREFIX/etc/apt/sources.list.d/science.list
 pkg up -y
-pkg i git nodejs yarn -y
-npm install -g pm2
-yarn global add pm2
-git clone https://github.com/elecV2/elecV2P.git
-cd ~/elecV2P
-yarn
-cd ~
-echo "cd ~/elecV2P && PORT=8000 node index.js" > v2p.sh
-chmod +x v2p.sh
+pkg i golang -y
+go get -u -v github.com/Sansui233/proxypool
+proxypool=$(ls ~/go/pkg/mod/github.com/'!sansui233')
+chmod -R 777 ~/go/pkg/mod/github.com/'!sansui233'
+cp -r ~/go/pkg/mod/github.com/'!sansui233'/$proxypool ~/proxypool
+rm -rf ~/go/pkg/mod/github.com/'!sansui233' ~/proxypool/config/config.yaml ~/proxypool/config/source.yaml
+config=$(curl -Ls https://raw.githubusercontent.com/shoujiyanxishe/proxypoolsh/main/termux/config.yaml) && echo "$config" > ~/proxypool/config/config.yaml
+source=$(curl -Ls https://raw.githubusercontent.com/shoujiyanxishe/proxypoolsh/main/termux/source.yaml) && echo "$source" > ~/proxypool/config/source.yaml
+echo "cd ~/proxypool && go run main.go -c ./config/config.yaml" > proxypool.sh
+chmod +x proxypool.sh
 clear
-echo v2p已经安装完成，输入./v2p.sh启动
+echo proxypool安装完成，输入./proxypool.sh启动
