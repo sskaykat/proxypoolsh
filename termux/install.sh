a0e315db59e90a52f5b9ad87242169522ad4128d
shoujiyanxishe <82391323+shoujiyanxishe@users.noreply.github.com>
2022-03-29 17:40:15 +0800

    Update install.sh

--- termux/install.sh
+++ termux/install.sh
@@ -10,8 +10,8 @@ wget https://github.com/Sansui233/proxypool/archive/refs/tags/v$last_version.zip
 rm -rf proxypool-$last_version
 unzip v$last_version.zip
 rm -rf v$last_version.zip ~/proxypool-$last_version/config/config.yaml ~/proxypool-$last_version/config/source.yaml
-config=$(curl -Ls https://raw.githubusercontent.com/shoujiyanxishe/proxypool/main/termux/config.yaml) && echo "$config" > ~/proxypool-$last_version/config/config.yaml
-source=$(curl -Ls https://raw.githubusercontent.com/shoujiyanxishe/proxypool/main/termux/source.yaml) && echo "$source" > ~/proxypool-$last_version/config/source.yaml
+wget -O ~/proxypool-$last_version/config/config.yaml https://raw.githubusercontent.com/shoujiyanxishe/proxypool/main/termux/config.yaml
+wget -O ~/proxypool-$last_version/config/source.yaml https://raw.githubusercontent.com/shoujiyanxishe/proxypool/main/termux/source.yaml
 echo "cd ~/proxypool-$last_version && go run main.go -c ./config/config.yaml" > proxypool.sh
 chmod +x proxypool.sh
 clear
